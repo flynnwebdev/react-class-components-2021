14feb017c0a5efa29352a5af6f39aa5314dbec99
Flynn <flynnwebdev@gmail.com>
2021-01-12 12:25:06 +1000

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,14 @@ import React from "react"
 import Clock from "./Clock"
 
 const App = () => {
-	return <Clock date={new Date()} />
+	const plus8 = new Date()
+	plus8.setHours(plus8.getHours() - 8)
+	return (
+		<>
+			<Clock date={new Date()} />
+			<Clock date={plus8} zone="Bulgaria" />
+		</>
+	)
 }
 
 export default App
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,32 +1,86 @@
 import React from "react"
 import "./Clock.css"
 
-const Clock = props => {
-	const { date } = props
-	const seconds = date.getSeconds()
-	const minutes = date.getMinutes()
-	const hours = date.getHours()
+class Clock extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			date: props.date,
+			foo: props.zone || "Local Time"
+		}
+	}
 
-	return (
-		<div className="container">
-			<div className="clock-face">
-				<div className="clock">
-					<div className="hours-container">
-						<div
-							className="hours"
-							style={{ transform: `rotateZ(${hours * 30 + minutes / 2}deg)` }}
-						></div>
-					</div>
-					<div className="minutes-container">
-						<div className="minutes" style={{ transform: `rotateZ(${minutes * 6}deg)` }}></div>
-					</div>
-					<div className="seconds-container">
-						<div className="seconds" style={{ transform: `rotateZ(${seconds * 6}deg)` }}></div>
+	componentDidMount() {
+		this.timerId = setInterval(this.tick, 1000) 
+		// this.setState({ foo: "Hi!" })
+	}
+
+	componentDidUpdate() {
+		console.log("Clock updated.")
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerId)
+	}
+
+	tick = () => {
+		const newDate = this.state.date
+		newDate.setSeconds(newDate.getSeconds() + 1)
+		this.setState({ date: newDate })
+		// this.state.date = new Date()
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<h1>{this.state.foo}</h1>
+				<div className="clock-face">
+					<div className="clock">
+						<div className="hours-container">
+							<div
+								className="hours"
+								style={{ transform: `rotateZ(${this.state.date.getHours() * 30 + this.state.date.getMinutes() / 2}deg)` }}
+							></div>
+						</div>
+						<div className="minutes-container">
+							<div className="minutes" style={{ transform: `rotateZ(${this.state.date.getMinutes() * 6}deg)` }}></div>
+						</div>
+						<div className="seconds-container">
+							<div className="seconds" style={{ transform: `rotateZ(${this.state.date.getSeconds() * 6}deg)` }}></div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
+
+// const Clock = props => {
+// 	const { date } = props
+// 	const seconds = date.getSeconds()
+// 	const minutes = date.getMinutes()
+// 	const hours = date.getHours()
+
+// 	return (
+// 		<div className="container">
+// 			<div className="clock-face">
+// 				<div className="clock">
+// 					<div className="hours-container">
+// 						<div
+// 							className="hours"
+// 							style={{ transform: `rotateZ(${hours * 30 + minutes / 2}deg)` }}
+// 						></div>
+// 					</div>
+// 					<div className="minutes-container">
+// 						<div className="minutes" style={{ transform: `rotateZ(${minutes * 6}deg)` }}></div>
+// 					</div>
+// 					<div className="seconds-container">
+// 						<div className="seconds" style={{ transform: `rotateZ(${seconds * 6}deg)` }}></div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	)
+// }
 
 export default Clock
